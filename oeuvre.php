453a77e96a2bd6e84a95ce76b02edd9cdef56939
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-09-26 21:45:25 +0200

    refactor: étape 4
    - request a single entry instead of the entire database

--- oeuvre.php
+++ oeuvre.php
@@ -8,20 +8,18 @@
     }
 
     $db = DBConnection();
-    $oeuvres = $db->query('SELECT * FROM oeuvres');
-    $oeuvre = null;
+    $stmt = $db->prepare('SELECT * FROM oeuvres WHERE id = :id');
+    $isSuccess = $stmt->execute([ ':id' => intval($_GET['id']) ]);
 
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
+    if(!$isSuccess) {
+        header('Location: index.php');
+        exit();
     }
 
+    $oeuvre = $stmt->fetch(\PDO::FETCH_ASSOC);
+
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
+    if(!$oeuvre) {
         header('Location: index.php');
         exit();
     }
