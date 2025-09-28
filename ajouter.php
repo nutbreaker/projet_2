dd5606f4042d7eb08c8bdc015eae4306f7166a8e
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-09-28 13:39:54 +0200

    refactor(ajouter): move add oeuvre logic
    The motivation behind this refactor is to have the "add" logic
    in the same place.

--- ajouter.php
+++ ajouter.php
@@ -1,6 +1,53 @@
-<?php require_once 'header.php'; ?>
+<?php 
 
-<form action="traitement.php" method="POST">
+require_once 'header.php'; 
+require_once 'database.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $isLengthValid = function (string $value, $len = 0) {
+        return isset($value) && strlen(trim($value)) > $len;
+    };
+    $formFieldsValidator = [
+        'titre' => [
+            'errorMessage' => 'Le champ titre ne peut être vide.',
+            'rule' =>  $isLengthValid
+        ],
+        'artiste' =>  [
+            'errorMessage' => 'Le champ titre ne peut être vide.',
+            'rule' =>  $isLengthValid
+        ],
+        'image' =>  [
+            'errorMessage' => 'Le champ image doit être une URL.',
+            'rule' =>  function (string $value) {
+                return isset($value) && filter_var($value, FILTER_VALIDATE_URL);
+            }
+        ],
+        'description' =>  [
+            'errorMessage' => 'Le champ description doit contenir au moins 3 caractères.',
+            'rule' => function (string $value) use ($isLengthValid) {
+                return $isLengthValid($value, 3);
+            }
+        ],
+    ];
+    $errorMessages = [];
+    $fields = [];
+
+    foreach ($formFieldsValidator as $key => $validator) {
+        if (!$validator['rule']($_POST[$key])) {
+            $errorMessages[$key] = $validator['errorMessage'];
+        }
+
+        $fields[$key] = htmlentities($_POST[$key]);
+    }
+
+    if (empty($errorMessages) && DataRepository::insertOeuvre($fields)) {
+        header('Location: index.php');
+        exit();
+    }
+}
+?>
+
+<form action="ajouter.php" method="POST">
     <div class="champ-formulaire">
         <label for="titre">Titre de l'œuvre</label>
         <input type="text" name="titre" id="titre"
