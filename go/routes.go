2dbe90afe58552bd2cb7906bf3224ba402d0f6fc
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-10-26 14:45:24 +0100

    feat(accueil): display all available `oeuvres`
    Add the logic to display all available `oeuvres`.
    
    - parses html templates used to display the `oeuvres`
    - get `oeuvres` from the database
    - pass the result to the html template

--- go/routes.go
+++ go/routes.go
@@ -1,9 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
-func (a *artBox) home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("home"))
+func (a *artBox) accueil(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles(
+		"./templates/base.html",
+		"./templates/accueil.html",
+	)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	oeuvres, err := a.GetOeuvres()
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	err = tmpl.ExecuteTemplate(w, "base", map[string]any{
+		"oeuvres": oeuvres,
+	})
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func (a *artBox) oeuvre(w http.ResponseWriter, r *http.Request) {
